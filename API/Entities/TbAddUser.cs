d7b2946fe8449dbbc590d34b75f48f609fc47ec5
Vijay-Kumavat <kumavatvijay776@gmail.com>
2021-10-04 23:42:09 +0530

    Add the migrations

--- API/Entities/TbAddUser.cs
+++ API/Entities/TbAddUser.cs
@@ -1,9 +1,10 @@
 namespace API.Entities
 {
-    public class AddUser
+    public class TbAddUser
     {
         public int Id { get; set; }
-        
         public string UserName { get; set; }
+        
+        
     }
 }
